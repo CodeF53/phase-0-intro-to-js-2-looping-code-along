8e2591888a76468d8bb333d54978771608c5fb76
Chase Welsh <fseusb@gmail.com>
2022-06-28 10:33:14 -0600

    commit tree dda939dd242610243c12fb73f0494400a84caf1e

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+function writeCards(nameArray, eventName) {
+    let cardArray = [];
+
+    for (let i = 0; i < nameArray.length; i++) {
+        cardArray.push(`Thank you, ${nameArray[i]}, for the wonderful ${eventName} gift!`);
+    }
+    
+    return cardArray;
+}
+
+function countDown(start) {
+    let count = start;
+
+    while (count>=0) {
+        console.log(count);
+        count--;
+    }
+}
